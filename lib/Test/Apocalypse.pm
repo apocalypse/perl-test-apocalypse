1b02f5e516bb98656b30b7f1cdc95d854db4c871
Apocalypse <perl@0ne.us>
2010-02-24 23:25:52 -0700

    add note about LICENSE

--- lib/Test/Apocalypse.pm
+++ lib/Test/Apocalypse.pm
@@ -378,4 +378,6 @@ Copyright 2010 by Apocalypse
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
 
+The full text of the license can be found in the LICENSE file included with this module.
+
 =cut
